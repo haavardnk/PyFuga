e8f8fe341915ba9ed68fe52ed37e3e117727c0f6
Mads M. Pedersen <mmpe@dtu.dk>
2023-06-14 10:09:52 +0000

    add function to calculate z0 from ti (stability dependent)

---
+++ pyfuga/tests/test_z0.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pyfuga.tests import npt
+from pyfuga.z0 import phi, psi, z0_from_TI
+
+
+def test_phi():
+    zeta = np.linspace(-.1, .1, 11)
+    print(list(np.round(phi(zeta), 3)))
+    if 0:
+        plt.plot(zeta, phi(zeta))
+        plt.show()
+    npt.assert_array_almost_equal(phi(zeta), [0.764, 0.792, 0.825, 0.867, 0.922, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5], 3)
+
+
+def test_psi():
+    zeta = np.linspace(-.1, .1, 51)
+    print(list(np.round(psi(zeta[::5]), 3)))
+    print(list(np.round(psi(zeta[::5], 'Wilson'), 3)))
+    if 0:
+        plt.plot(zeta, psi(zeta))
+        plt.plot(zeta, psi(zeta, 'Wilson'), label='Wilson')
+        plt.legend()
+        plt.show()
+    npt.assert_array_almost_equal(psi(zeta[::5]),
+                                  [0.326, 0.276, 0.221, 0.159, 0.087, 0.0, -0.1, -0.2, -0.3, -0.4, -0.5], 3)
+    npt.assert_array_almost_equal(psi(zeta[::5], 'Wilson'),
+                                  [2.541, 2.488, 2.427, 2.355, 2.261, 0.0, -0.1, -0.2, -0.3, -0.4, -0.5], 3)
+
+
+def test_z0_from_TI():
+    if 0:
+        TI = np.linspace(3, 10, 100) / 100
+        zref = 70
+        plt.figure()
+
+        z0_stable = z0_from_TI(TI, zref, 6e-7)
+        z0_neutral = z0_from_TI(TI, zref, 0.0)
+        z0_unstable = z0_from_TI(TI, zref, -6e-7)
+        plt.plot(TI * 100, z0_stable, label='stable')
+        plt.plot(TI * 100, z0_neutral, label='neutral')
+        plt.plot(TI * 100, z0_unstable, label='unstable')
+
+        z0_unstable = z0_from_TI(TI, zref, -6e-7)
+        plt.plot(TI * 100, z0_unstable, '--', label='unstable')
+
+        plt.xlabel('ti%')
+        plt.ylabel('z0')
+        plt.legend()
+        plt.show()
+        print(np.array([z0_from_TI]))
+    npt.assert_array_almost_equal([z0_from_TI([.06, .12], 70, zeta0) for zeta0 in [-6e-7, 0, 6e-7]],
+                                  [[1e-05, 1.663e-02], [1e-05, 1.683e-02], [7.269e-05, 1.703e-02]], 5)
---
+++ pyfuga/z0.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.special import lambertw
+from scipy.optimize import fsolve
+
+Cm1 = 5
+Cm2 = -19.3
+
+
+def phi(zeta):
+    zeta = np.atleast_1d(zeta)
+    return np.where(zeta <= 0, (1 + Cm2 * zeta)**-.25, 1 + Cm1 * zeta)
+
+
+def psi(zeta, unstable=''):
+    zeta = np.atleast_1d(zeta)
+    ind = zeta < 0
+    psi_n = np.zeros(zeta.shape)
+    aux = phi(zeta)**-1
+    if unstable == 'Wilson':
+        psi_n[ind] = 3 * np.log((1 + (1 + 3.6 * np.abs(zeta[ind])**(2 / 3))**.5))
+    else:
+        aux2 = (1.0 + aux[ind])**2 * (1 + aux[ind]**2)
+        psi_n[ind] = -np.log(8.0 / aux2) - 2.0 * np.arctan(Cm2 * zeta[ind] / aux2)
+    psi_n[~ind] = 1 - aux[~ind]**-1
+    psi_n[zeta == 0] = 0
+    return psi_n
+
+
+def z0_from_TI(TI, zref, zeta0, z0_limit=1e-5):
+    TI = np.atleast_1d(TI)
+    if zeta0 == 0:
+        z0 = zref * np.exp(-1.0 / TI)  # Paul's formula
+    elif zeta0 > 0:
+        # analytical expression from residual for stable conditions:
+        # 1/ti = np.log(zref/z0)+Cm1*zeta0(zref/z0-1)
+        a = Cm1 * zeta0
+        b = 1 / TI
+        x = np.real(lambertw(a * np.exp(a + b))) / a
+        z0 = zref / x
+    else:
+        # I had to solve it numerically for unstable conditions
+        x = np.array([fsolve(lambda x: x / np.exp(psi(x * zeta0) - psi(zeta0)) -
+                     np.exp(1 / ti), zref / 0.001)[0] for ti in TI])
+        z0 = zref / x
+    z0[z0 < z0_limit] = z0_limit
+    return z0
